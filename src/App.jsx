6034ef796cc7fca99ccf461ca310ff3517d624b8
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-29 16:33:15 +0530

    styles: added colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app-container">
+      <p>Created by Prathamesh Belurkar</p>
       <h1>Web blocker is triggered</h1>
+      <h2>Now access sites</h2>
     </div>
   );
 }
